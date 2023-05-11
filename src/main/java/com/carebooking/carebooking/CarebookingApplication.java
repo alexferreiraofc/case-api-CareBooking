dc3042243b616d738e4da536e074a48b0c381615
alexferreiraofc <alex.ferreiraofc@gmail.com>
2023-05-11 16:05:13 -0300

    Testing app if it works with the new spring-boot version.

--- src/main/java/com/carebooking/carebooking/CarebookingApplication.java
+++ src/main/java/com/carebooking/carebooking/CarebookingApplication.java
@@ -2,15 +2,12 @@ package com.carebooking.carebooking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class CarebookingApplication {
 
-
-	public static void main(String[] args) {
-		SpringApplication.run(CarebookingApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CarebookingApplication.class, args);
+    }
 
 }
--- src/main/java/com/carebooking/carebooking/api/PessoaController.java
+++ src/main/java/com/carebooking/carebooking/api/PessoaController.java
@@ -1,4 +1,15 @@
 package com.carebooking.carebooking.api;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class PessoaController {
+
+    @GetMapping(path = "/")
+    public String home() {
+        return "Hello World!";
+    }
+
+
 }
