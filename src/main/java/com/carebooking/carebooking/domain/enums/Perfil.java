978787781ef10bfc25734a02e1489a6ee7ce0f35
alexferreiraofc <alex.ferreiraofc@gmail.com>
2023-05-12 11:09:35 -0300

    feat: implemented Perfil for Pessoa

---
+++ src/main/java/com/carebooking/carebooking/domain/enums/Perfil.java
@@ -0,0 +1,35 @@
+package com.carebooking.carebooking.domain.enums;
+
+public enum Perfil {
+    PACIENTE(0, "Paciente"),
+    MEDICO(1, "MEDICO"),
+    COLABORADOR(2, "Colaborador");
+
+    private Integer codigo;
+    private final String cargo;
+
+    Perfil(Integer codigo, String cargo) {
+        this.cargo = cargo;
+        this.codigo = codigo;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public Integer getCodigo() {
+        return codigo;
+    }
+
+    public static Perfil toEnum(Integer cod) {
+        if (cod == null) {
+            return null;
+        }
+        for (Perfil funcao : Perfil.values()) {
+            if (cod.equals(funcao.getCodigo())) {
+                return funcao;
+            }
+        }
+        throw new IllegalArgumentException("Perfil inválido!");
+    }
+}
--- src/main/java/com/carebooking/carebooking/domain/model/Pessoa.java
+++ src/main/java/com/carebooking/carebooking/domain/model/Pessoa.java
@@ -1,5 +1,25 @@
 package com.carebooking.carebooking.domain.model;
 
+import jakarta.annotation.Nullable;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotNull;
+
+import java.util.Date;
+
+@Entity
 public class Pessoa {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotNull
+    private String nome;
+    @NotNull
+    private String cpf;
+    @NotNull
+    private Date dataNascimento;
 
 }
